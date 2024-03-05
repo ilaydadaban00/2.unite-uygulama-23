23d7aaf0a80dd89df7c44a77a2dd44d37b54929f
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-06 01:57:28 +0300

    Add files via upload

---
+++ 2.unite uygulama 24/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_uygulama_24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 100; i <= 150; i = i + 5)
+            {
+                comboBox1.Items.Add(i);
+            }
+        }
+    }
+}
